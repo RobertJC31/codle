8b4de71d53778c9193d09835e8970ae67def7e13
Robert Ceriani <63567533+RobertJC31@users.noreply.github.com>
2024-02-27 12:01:40 -0700

    init

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Codle</title>
+    <?php require 'resources/navbar.php' ?>
 </head>
 
 <html lang="en-US">
---
+++ resources/navbar.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+    <title>Codle</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<html lang="en-US">
+
+</html>

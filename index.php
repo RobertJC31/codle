7fbc0b39b4ced97b485335b7e3d718da87b6951c
Bowen Carnell <bowencarnell@u.boisestate.edu>
2024-02-28 09:57:16 -0700

    pop up message

--- index.php
+++ index.php
@@ -8,10 +8,26 @@
 
 <html lang="en-US">
     <body>
-
+        <div id="pop-up" class="pop-up">
+            <div class="pop-up-content">
+            <span class="close">&times;</span>
+            <p>Welcome to Codle!</p>
+            <p>To play Codle you have to guess the 4 digit code in 5 guesses.</p>
+        </div>
+        </div>
     </body>
 </html>
 
 <script>
     <?php require 'resources/scripts/codle.js' ?>
+    var popUp = document.getElementById("pop-up");
+    window.onload = function() 
+    {
+        popUp.style.display = "block";
+    };
+    window.onclick = function(event) {
+        if (event.target == popUp) {
+            popUp.style.display = "none";
+        }
+    };
 </script>

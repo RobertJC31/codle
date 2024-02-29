e36b54adeb4f85d0c213b41eb814f8660ec8f308
Bowen Carnell <bowencarnell@u.boisestate.edu>
2024-02-29 10:16:38 -0700

    Womp Womp

--- index.php
+++ index.php
@@ -7,6 +7,21 @@
 </head>
 
 <html lang="en-US">
+    <style>
+        .board {
+            display: grid;
+            grid-template-columns: repeat(10, 30px);
+            grid-template-rows: repeat(1, 30px);
+            gap: 1px;
+            border: 1px solid #000;
+        }
+
+        .peg {
+            width: 30px; 
+            height: 30px; 
+            background-color: #ccc; 
+        }
+    </style>
     <body>
         <div id="pop-up" class="pop-up">
             <div class="pop-up-content">
@@ -15,6 +30,9 @@
             <p>To play Codle you have to guess the 4 digit code in 5 guesses.</p>
         </div>
         </div>
+        <div class="board">
+
+    </div>
     </body>
 </html>
 

de1187adeb0e5d257b10a733cfde0822baaf356f
Robert Ceriani <63567533+RobertJC31@users.noreply.github.com>
2024-02-29 14:19:48 -0700

    Updated Popup Sctipt to take boolean argument to include the specified trigger element's child or not

--- resources/scripts/popup.js
+++ resources/scripts/popup.js
@@ -1,10 +1,10 @@
-function addListenerToTriggerDisplay(triggerElementID, targetElementID, style) {
+function addListenerToTriggerDisplay(triggerElementID, targetElementID, style, includeChildren) {
     let triggerElement = document.getElementById(triggerElementID);
     let targetElement = document.getElementById(targetElementID);
 
     if (triggerElement && targetElement) {
         triggerElement.addEventListener('click', function(event) {
-            if (triggerElement !== event.target) {
+            if ((triggerElement !== event.target) && !(includeChildren)) {
                 return;
             }
             targetElement.style.display = style;

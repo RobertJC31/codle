fae93b32370480963caf4dd4d2b1b007afacf867
Robert Ceriani <63567533+RobertJC31@users.noreply.github.com>
2024-02-29 09:42:57 -0700

    Updated Popup Script disallows children from inheriting parent triggers.

--- resources/scripts/popup.js
+++ resources/scripts/popup.js
@@ -1,10 +1,14 @@
 function addListenerToTriggerDisplay(triggerElementID, targetElementID, style) {
-    const triggerElement = document.getElementById(triggerElementID);
-    const targetElement = document.getElementById(targetElementID);
+    let triggerElement = document.getElementById(triggerElementID);
+    let targetElement = document.getElementById(targetElementID);
 
     if (triggerElement && targetElement) {
-        triggerElement.addEventListener('click', function() {
+        triggerElement.addEventListener('click', function(event) {
+            if (triggerElement !== event.target) {
+                return;
+            }
             targetElement.style.display = style;
+
         })
     } else {
         console.error("One of the specified elements not found.");

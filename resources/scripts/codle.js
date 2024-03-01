691d1847e17fa3ee77e54a1642d715691f6fb1a6
Robert Ceriani <63567533+RobertJC31@users.noreply.github.com>
2024-03-01 00:01:47 -0700

    Updated Code Generator

--- resources/scripts/codle.js
+++ resources/scripts/codle.js
@@ -1,17 +1,21 @@
-const code = [];
 alphaChar = 'abcdefghijklmnopqrstuvwxyz';
 
-function generateCode() {
+function generateCode(allowAlpha, allow5Digits) {
+    let code = [];
     let alphas = 0;
-    for (let i = 0; i < 5; i++) {
-        if (((alphas <= 1) && (Math.random() < 0.5))) {
+    let numDigits = 4;
+
+    if (allow5Digits === true) {
+        numDigits = 5;
+    }
+
+    for (let i = 0; i < numDigits; i++) {
+        if (((alphas <= 1) && (Math.random() < 0.5)) && allowAlpha) {
             code.push(alphaChar.charAt(Math.floor(Math.random() * 26)));
             alphas++;
         } else {
             code.push(Math.floor(Math.random() * 10));
         }
     }
+    return code;
 }
-
-generateCode();
-console.log(code);
